7b6966a0449c42d716dfa312c2b12cc0c8717076
WilberCode <parionahuaraca@gmail.com>
2019-06-18 01:28:59 -0500

    Updates

---
+++ precache-manifest.3feaf9b5290804e1c6bb0b3f14f3a77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb488a6f836f5034e68095466eb22fd5",
+    "url": "/react-01/index.html"
+  },
+  {
+    "revision": "a74bd4624f64ce24b051",
+    "url": "/react-01/static/css/main.733d9d74.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-01/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a74bd4624f64ce24b051",
+    "url": "/react-01/static/js/main.3c8ae96c.chunk.js"
+  },
+  {
+    "revision": "0abe58480b2aaf746ea3",
+    "url": "/react-01/static/js/runtime~main.4eef8156.js"
+  }
+]);
